015c7f21a53216f6dc4a531446e214bf550c5848
Valerio <valerio.delmeglio@gmail.com>
2014-02-24 22:13:02 +0100

    Adding some comments to the code

--- moviesRating.py
+++ moviesRating.py
@@ -9,6 +9,8 @@ movieDb_api = 'http://api.themoviedb.org/3/search/movie?api_key={key}&query={tit
 key = '935be2f95e54dddddc67f6ee8953c6c0'
 poster_url='http://image.tmdb.org/t/p/original{url_to_poster}'
 
+# Changing the behavior of dicts, creating a subclass that  
+# automatically store duplicated values in lists under the same key.
 class Dictlist(dict):
     def __setitem__(self, key, value):
         try:
@@ -24,17 +26,20 @@ def movieInfoFinder():
     bbc_json_request=requests.get(bbc_url)
     bbc_json=bbc_json_request.json()
     
+    # Iterating through the movie titles to get all the infos 
+    # needed from the movieDb API and the bbc json
     for i in range(len(bbc_json['episodes'])):
  
-        title=bbc_json['episodes'][i]['programme']['title']
-        movie_db=json.load(urllib2.urlopen(movieDb_api.format(key=key,title=urllib2.quote(title))))
-        plot=bbc_json['episodes'][i]['programme']['short_synopsis']
+        title = bbc_json['episodes'][i]['programme']['title']
+        movie_db = json.load(urllib2.urlopen(movieDb_api.format(key=key,title=urllib2.quote(title))))
+        plot = bbc_json['episodes'][i]['programme']['short_synopsis']
 
         if movie_db['results']:
             rating[title] = movie_db['results'][0]['vote_average']
             rating[title] = plot
             rating[title] = poster_url.format(url_to_poster=movie_db['results'][0]['poster_path'])
             
+        # If the movie title is not found in the movieDb    
         else:
             rating[title] = 'Not found in the db' 
             rating[title] = plot 
